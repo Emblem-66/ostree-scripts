0488aeaf0d802e83f3ce5856e234abd9474ec447
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-06 20:32:21 +0100

    Create steam.sh

---
+++ general/steam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpmfusion=(
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+)
+rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+
+install_packages=(
+    "steam"
+)
+rpm -q ${install_packages[@]} || rpm-ostree install ${install_packages[@]}
