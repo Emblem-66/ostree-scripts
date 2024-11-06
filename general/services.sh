5023190f03c251c77050e07609c76a3eb6b137bb
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-06 22:00:56 +0100

    Update services.sh

--- general/services.sh
+++ general/services.sh
@@ -3,7 +3,26 @@
 set -ouex pipefail
 
 systemctl enable rpm-ostreed-automatic.timer
+
+cat <<EOF | sudo tee /etc/systemd/system/flatpak-update.service > /dev/null
+[Unit]
+Description=Update Flatpaks
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/flatpak update -y
+[Install]
+WantedBy=default.target
+EOF
 systemctl enable flatpak-update.service
+
+cat <<EOF | sudo tee /etc/systemd/system/flatpak-update.timer > /dev/null
+[Unit]
+Description=Update Flatpaks
+[Timer]
+OnCalendar=*:0/4
+Persistent=true
+[Install]
+WantedBy=timers.target
+EOF
 systemctl enable flatpak-update.timer
-systemctl enable dconf-update.service
-systemctl enable flatpak-add-flathub-repo.service
+
